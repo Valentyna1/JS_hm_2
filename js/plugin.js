c922340d45fbeca8f5e32b4280e86a527acaae3d
Valentyna1 <valentynaleliukh@gmail.com>
2017-04-10 00:33:34 +0300

    first commit

--- js/plugin.js
+++ js/plugin.js
@@ -1,204 +1,3 @@
-//---------------------------------------ФУНКЦИИ----------------------------
-
-// // Задание 1
-// function multiplay() {
-// 	if (arguments.length == 0) return 0;
-// 	var res = 1;
-// 	for ( i=0, max = arguments.length; i < max; i++){
-// 		res *= arguments[i];
-// 	}
-// 	return res;
-// }
-
-// // Задание 2
-// function foo(limit) {
-// 	var value;
-// 	if(limit > 1){
-// 		value = limit * foo(limit-1);
-// 	} else{
-// 		value = limit;
-// 	}
-// 	return value;
-// }
-
-// // Задание 3
-// function reverseString(str) {
-//     var newStr = '';
-//     for (var i = str.length-1; i>=0; i--){
-//         newStr += str[i];
-//     }
-//     return newStr;
-// }
-// var res = reverseString('test');
-// console.log(res);
-
-
-// // Задание 4
-// function charCodeAt(str) {
-//     var newStr = '';
-// 	for (var i = 0; i <= str.length-1; i++){
-//         newStr += str[i].charCodeAt() + ' ';
-// 	}
-// 	return newStr;
-// }
-// var res = charCodeAt('hello');
-// console.log(res);
-
-// //Задание 5
-// function foo(str) {
-//     for (var i =0; i <= str.length-1; i++) {
-//         console.log(str[i]);
-//     }
-// }
-// var res = foo('test');
-
-// //Задание 6-1
-// function firstFunc(arr, func) {
-//     var newStr = 'New value: ';
-//     for (var i = 0; i < arr.length; i++) {
-//         newStr += func(arr[i]);
-//     }
-//     return newStr;
-// }
-// function secondFunc(one) {
-//     return one[0].toUpperCase() + one.substring(1);
-// }
-// var res = firstFunc(['my', 'name', 'is', 'trinity'], secondFunc);
-// console.log(res);
-
-// //Задание 6-2
-// function firstFunc(arr, func) {
-//     var newStr = 'New value: ';
-//     for (var i = 0; i < arr.length; i++) {
-//         newStr += func(arr[i]);
-//     }
-//     return newStr;
-// }
-// function secondFunc(two) {
-//     return two*10 + ',';
-// }
-// var res = firstFunc([10, 20, 30], secondFunc);
-// console.log(res.slice(0, length-1));
-
-// //Задание 6-3
-// function firstFunc(arr, func) {
-//     var newStr = 'New value: ';
-//     for (var i = 0; i < arr.length; i++) {
-//         newStr += func(arr[i]);
-//     }
-//     return newStr;
-// }
-// function secondFunc(three) {
-//     return `${three.name} is ${three.age}` + '; ';
-// }
-// var res = firstFunc([{age: 45, name: 'John'}, {age: 20, name: 'Aaron'}], secondFunc);
-// console.log(res);
-
-// //Задание 6-4
-// function firstFunc(arr, func) {
-//     var newStr = 'New value: ';
-//     for (var i = 0; i < arr.length; i++) {
-//         newStr += func(arr[i]);
-//     }
-//     return newStr;
-// }
-// function secondFunc(four) {
-//     return four + ', ';
-// }
-// var res = firstFunc(['abc', '123'], secondFunc);
-// console.log(res.slice(0, length-1));
-
-
-//---------------------------------------МАССИВЫ----------------------------
-
-// //Задание 1
-// var str = 'bcdaeflmjgkhi',
-// 	newStr = '';
-// for (var i = str.length-1; i >= 0; i--) {
-// 	newStr += str[i] + ' ';
-// }
-// arr = newStr.split(' ');
-// var newArr = arr.sort().reverse();
-// strLast = newArr.join('');
-// console.log(strLast);
-
-// //Задание 2
-// var arr = [2, 4, 7, 1, -2, 10, -9];
-// function sortArr(prev, next) {
-// 	console.log(next, prev);
-// 	return prev - next;
-// }
-// var newArr = arr.sort(sortArr);
-// console.log(newArr);
-
-// //Задание 3
-// var arr = ['a', 'b', 'c', 'd', 'e', 'f'];
-// var newArr = arr.slice(2,5);
-// console.log(newArr);
-
-// //Задание 4
-// var arr = ['one', 2, 'three', 4];
-// var newArr = arr.concat(arr);
-// console.log(newArr);
-
-// //Задание 5
-// var arr = [1, 2, 3, 4, 5];
-// var newArr = arr.splice(2,2);
-// console.log(arr);
-
-// //Задание 6
-// var arr = [1, 2, 3, 4, 5];
-// var newArr = arr.splice(2,2,'three','four');
-// console.log(arr);
-
-// // Задание 7
-// var arr = ['I', 'am', 'an', 'array'];
-// var newArr = arr.splice(3,0,'awesome');
-// console.log(arr);
-
-// // Задание 8
-// var arr = [[14,45], [1], ['a', 'c', 'd']];
-// var newArr = arr.sort(length);
-// console.log(newArr);
-
-// // Задание 9
-// var arr = ['a', 'b', 'c', 'd', 'e', 'f'];
-// var newArr = arr.slice(length).concat();
-// console.log(newArr);
-
-// // Задание 10
-// var arr = [
-// 	{cpu: 'Intel', info: {cores: 2, cache: 3}},
-//     {cpu: 'Intel', info: {cores: 4, cache: 4}},
-//     {cpu: 'Amd', info: {cores: 1, cache: 1}},
-//     {cpu: 'Intel', info: {cores: 3, cache: 2}},
-//     {cpu: 'Amd', info: {cores: 4, cache: 2}}
-//     ];
-// function sortArr(prev, next) {
-// 	console.log(next, prev);
-// 	return prev[arr.cores] - next[arr.cores];
-// }
-// var newArr = arr.sort(sortArr);
-// console.log(newArr);
-
-// // Задание 11
-// const products = [
-// 	{title: 'prod1', price: 5.2},
-//     {title: 'prod2', price: 15},
-//     {title: 'prod3', price: 18.9},
-//     {title: 'prod4', price: 19},
-//     {title: 'prod5', price: 0.18},
-//     {title: 'prod6', price: 25},
-//     {title: 'prod7', price: 8},
-//     {title: 'prod8', price: 63}
-// ];
-// for (let i = 0, max = products.length; i < max; i++){
-// 	if (products[i]['price'] >= 15 && products[i]['price'] <= 30){
-// 		console.log(products[i]);
-// 	}
-// }
-
-
 //---------------------------------------THIS----------------------------
 // // Задание 1
 // const rectangle = {
@@ -227,4 +26,130 @@
 // console.log(price.getPrice());
 // console.log(price.getPriceWithDiscount());
 
-// Задание 3
+// // Задание 3
+// const user = {name: 'Abraham'};
+// getUserName = function () {
+//     return user.name
+// };
+// console.log(getUserName());
+
+// // Задание 4
+// let obj = {height: 10};
+// getMoreOne = function () {
+//   return obj.height + 1;
+// };
+//
+// console.log(getMoreOne());
+
+// // Задание 5
+// const numerator = {
+//     value: 1,
+//     double: function () {
+//         return this.value * 2
+//     },
+//     plusOne: function () {
+//         return this.value + 1
+//     },
+//     minusOne: function () {
+//         return this.value - 1
+//     }
+// };
+// console.log(`Функция удвоения дает результат ${numerator.double()}`);
+// console.log(`Функция +1 дает результат ${numerator.plusOne()}`);
+// console.log(`Функция -1 дает результат ${numerator.minusOne()}`);
+//
+// console.log(numerator.double() + numerator.plusOne() + numerator.minusOne());
+
+// // Задание 6
+// const user = {name: 'Abraham'},
+//     otherUser = {name: 'John',
+//     getName: function () {
+//         return this.name
+//     }
+// };
+// console.log(otherUser.getName());
+
+//-------------------Part 2-------------
+// // Задание 1
+// function getList() {
+//     return this.list
+// }
+// let users = {
+//     lenght: 4,
+//     list: ['Abraham', 'James', 'John', 'Steven']
+// };
+// console.log(getList()); /* undefined */
+// users.getList = getList;
+// console.log(users.getList()); /* массив из имен */
+// console.log(getList.call(users)); /* массив из имен */
+
+// // Задание 2
+// let object = {
+//     price: 10,
+//     quantity: 5,
+//     getMultiply: getMultiply
+// };
+// function getMultiply() {
+//     return this.price * this.quantity
+// }
+// console.log(object.getMultiply());
+
+// // Задание 3
+// let object = {
+//     price: 10,
+//     quantity: 5,
+//     getMultiply: getMultiply
+// };
+// function getMultiply() {
+//     return this.price * this.quantity
+// }
+// // console.log(object.getMultiply());
+//
+// let object2 = {
+//     price: 15,
+//     quantity: 10,
+//     getMultiply: getMultiply
+// };
+// console.log(object2.getMultiply());
+
+// // Задание 4
+// let sizes = {width: 5, height: 10};
+// var getSquare = function () {
+//     return this.width * this.height
+// };
+// console.log(getSquare.call(sizes));
+
+// // Задание 5
+// let numbers = [4, 12, 0, 10, -2, 4];
+// var res = Math.min.apply(null, numbers);
+// console.log(res);
+
+// // Задание 6
+// const element = {
+//     height: '15px',
+//     marginTop: '5px',
+//     marginBottom: '5px',
+//     getFullHeight: function () {
+//         return parseFloat(this.height) + parseFloat(this.marginTop) + parseFloat(this.marginBottom)
+//     }
+// };
+// console.log(element.getFullHeight());
+//
+// const block = {
+//     height: '5px',
+//     marginTop: '3px',
+//     marginBottom: '3px'
+// };
+// block.getFullHeight = element.getFullHeight;
+// console.log(block.getFullHeight());
+
+// Задание 7
+let element = {
+    height: 25,
+    getHeight: function () {
+        return this.height
+    }
+};
+
+let getElementHeight = element.getHeight;
+console.log(getElementHeight.call(element));
